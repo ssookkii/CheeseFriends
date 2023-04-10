1a5e30d7dcd16152ddb4d59bf6860e95a8006c73
ssookkii <sangsook4702@gmail.com>
2023-04-10 10:25:16 +0900

    기본 설정

---
+++ src/main/java/mul/cam/a/CheesefriendsBackApplication.java
@@ -0,0 +1,13 @@
+package mul.cam.a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheesefriendsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheesefriendsBackApplication.class, args);
+	}
+
+}
