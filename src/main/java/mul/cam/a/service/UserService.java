f55f3d1fcd3603b8ccc2bca58e72dca346985ad9
onlyoy <onlyoy4@naver.com>
2023-04-18 10:41:31 +0900

    Revert "Revert "psj2 0417 commit""
    This reverts commit fa6492587c02ebcf713ada1470438e43c26b804a.

--- src/main/java/mul/cam/a/service/UserService.java
+++ src/main/java/mul/cam/a/service/UserService.java
@@ -2,6 +2,7 @@ package mul.cam.a.service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import mul.cam.a.dao.UserDao;
 import mul.cam.a.dto.TestEduDto;
+import mul.cam.a.dto.UserDto;
+import mul.cam.a.util.naver2;
+
 
 @Service
 @Transactional
@@ -32,5 +36,41 @@ public class UserService {
 		
 		return dao.subjectlist(edu_code);
 	}
+	
+	public String eduname(String edu_code) {
+		System.out.println("UserService eduname() " + new Date());
+		
+		return dao.eduname(edu_code);
+	}
+	
+	public String subname(String sub_code) {
+		System.out.println("UserService subname() " + new Date());
+		
+		return dao.subname(sub_code);
+	}
+	
+	public String sendRandomMessage(String phone) {
+		naver2 message = new naver2();
+	    Random rand = new Random();
+	    String random = "";
+	    for (int i = 0; i < 6; i++) {
+	        String ran = Integer.toString(rand.nextInt(10));
+	        random += ran;
+	    }
+	    System.out.println("회원가입 문자 인증 => " + random);
 
+	    message.sendSMS(phone, random);
+
+	    return random;
+	}
+	
+	public boolean adduser(UserDto dto) {
+		int count = dao.adduser(dto);
+		return count>0?true:false;
+	}
+	
+	public boolean addusersubject(TestEduDto dto) {
+		int count = dao.addusersubject(dto);
+		return count>0?true:false;
+	}
 }
