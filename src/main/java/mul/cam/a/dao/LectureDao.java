a1edb735d742c201eb319e725dc4b30844dfa246
jyeonshin <94603624+jyeonshin@users.noreply.github.com>
2023-04-18 23:20:16 +0900

    LectureDao, Service, Controller, xml 수정 및 LectureDaoImpl, ServiceImpl 삭제

--- src/main/java/mul/cam/a/dao/LectureDao.java
+++ src/main/java/mul/cam/a/dao/LectureDao.java
@@ -1,8 +1,14 @@
 package mul.cam.a.dao;
 
 import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
 import mul.cam.a.dto.LectureDto;
 
+@Mapper
+@Repository
 public interface LectureDao {
 
 	// 강의 목록
