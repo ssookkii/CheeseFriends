69e4822c5faa0b3ddefc50d6c5a4df428c5e5871
jyeonshin <94603624+jyeonshin@users.noreply.github.com>
2023-04-20 11:39:13 +0900

    Lecture dao, controller, service 수정

--- src/main/java/mul/cam/a/dao/LectureDao.java
+++ src/main/java/mul/cam/a/dao/LectureDao.java
@@ -6,11 +6,18 @@ import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import mul.cam.a.dto.LectureDto;
+import mul.cam.a.dto.LectureParam;
 
 @Mapper
 @Repository
 public interface LectureDao {
 
 	// 강의 목록
-	List<LectureDto> lectureList(Integer seq);
+	List<LectureDto> lectureList(LectureParam param);
+	
+	// 수업 자료 등록
+	int writeLecture(LectureDto bbs);
+	
+	// 수업 자료 목록
+	List<LectureDto> learningList(LectureParam param);
 }
