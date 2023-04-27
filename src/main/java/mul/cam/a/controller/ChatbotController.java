f733e757d5ddafa9e0571e912ad24ca64fb41622
jyeonshin <94603624+jyeonshin@users.noreply.github.com>
2023-04-27 17:09:04 +0900

    04-27 파파고 번역 컨트롤러 커밋

--- src/main/java/mul/cam/a/controller/ChatbotController.java
+++ src/main/java/mul/cam/a/controller/ChatbotController.java
@@ -2,17 +2,22 @@ package mul.cam.a.controller;
 
 import java.util.Date;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import mul.cam.a.chatbot.Chatbot;
+import mul.cam.a.chatbot.PapagoTranslator;
 
 public class ChatbotController {
 	
-	@PostMapping(value = "chatBot")
+	@CrossOrigin(origins = "http://localhost:3000")
+	@PostMapping(value = "chatbot")
 	public String chatBot(String msg) {
 		System.out.println("NaverCloudController chatBot " +  new Date());
 		
 		String json = Chatbot.chatBot(msg);
 		return json;
 	}
+	
 }
