e1ff782480da901d1751c2c82d5cefd00d9ede86
ssookkii <sangsook4702@gmail.com>
2023-04-27 17:22:50 +0900

    0427

--- src/main/java/mul/cam/a/controller/ChatbotController.java
+++ src/main/java/mul/cam/a/controller/ChatbotController.java
@@ -2,24 +2,19 @@ package mul.cam.a.controller;
 
 import java.util.Date;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import mul.cam.a.chatbot.Chatbot;
-import mul.cam.a.chatbot.PapagoTranslator;
 
 @RestController
 public class ChatbotController {
 	
-	@CrossOrigin(origins = "http://localhost:3000")
-	@PostMapping(value = "chatbot")
+	@PostMapping(value = "chatBot")
 	public String chatBot(String msg) {
 		//System.out.println("NaverCloudController chatBot " +  new Date());
 		
 		String json = Chatbot.chatBot(msg);
 		return json;
 	}
-	
 }
